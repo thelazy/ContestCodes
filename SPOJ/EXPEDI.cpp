f826a8fa8e0d99cfa7544a40feb3bee52e16693f
thelazy <priyank.bpgc@gmail.com>
2017-03-22 15:29:36 +0530

    Changes to EXPEDITION SPOJ

--- SPOJ/EXPEDI.cpp
+++ SPOJ/EXPEDI.cpp
@@ -1,51 +1,60 @@
+/*
+Input:
+1
+4
+4 4
+5 2
+11 5
+15 10
+25 10
+
+Output:
+2
+*/
 #include <bits/stdc++.h>
 
 using namespace std;
 
+typedef long long ll;
+
 int main(){
-	long long t, n, a, b, i, l, p, ans;
+	long long int t, N, Distance, FuelAmount, iniPos, iniFuel, ans;
 	cin>>t;
 	while(t--){
 		priority_queue<long long> q;
-		cin>>n;
-		vector < pair<long long, long long> > v;
-		for(i=0; i<n; i++){
-			cin>>a>>b;
-			v.push_back(make_pair(a, b));
+		cin>>N;
+		vector < pair<long long, long long> > FuelStops;
+		for(ll i=0; i<N; i++){
+			cin>>Distance>>FuelAmount;
+			FuelStops.push_back(make_pair(Distance, FuelAmount));
 		}
-		v.push_back(make_pair(0,0));
-		cin>>l>>p;
-		sort(v.begin(), v.end());
+		FuelStops.push_back(make_pair(0,0));
+		cin>>iniPos>>iniFuel;
+		sort(FuelStops.begin(), FuelStops.end());
 		ans=0;
 		int flag=1;
-		for(i=v.size()-1; i>=0; i--){
-			//cout<<v[i].first<<" "<<v[i].second<<endl;
-			if(v[i].first<=l){
-				 if((l-p)<=v[i].first){
-				 	//cout<<"Pushing : "<<v[i].second<<endl;
-				 	q.push(v[i].second);
-				 }
-				 else{
-				 	while((l-p)>v[i].first){
-				 		if(!q.empty()){
-				 			p += q.top();
-				 			//cout<<"Adding : "<<q.top()<<endl;
-				 			q.pop();
-				 			ans++;
-				 		}
-				 		else{
-				 			flag=0;
-							break;
-				 		}
-				 	}
-				 	if(!flag)
-				 		break;
-				 	q.push(v[i].second);
-				 	//cout<<"Pushing : "<<v[i].second<<endl;
-				 }
+		for(ll i=FuelStops.size()-1; i>=0; i--){
+			 if(FuelStops[i].first >= (iniPos-iniFuel)){
+			 	q.push(FuelStops[i].second);
+			 }
+			 else{
+			 	while((iniPos-iniFuel)>FuelStops[i].first){
+					if(!q.empty()){
+			 			iniFuel += q.top();
+			 			q.pop();
+			 			ans++;
+			 		}
+				 	else{
+				 		flag=0;
+						break;
+			 		}
+			 	}
+			 	if(!flag)
+				 	break;
+				q.push(FuelStops[i].second);
 			}
 		}
-		if((l-p)<=0){
+		if((iniPos-iniFuel)<=0){
 			cout<<ans<<endl;
 		}
 		else
